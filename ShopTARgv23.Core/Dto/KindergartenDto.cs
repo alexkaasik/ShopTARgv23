be0ae1b84113213c2ebfb3c61547649823d04034
alexkaasik <aleks.kaasik@outlook.com>
2024-10-20 21:01:00 +0300

    Added all data entries to nullable

--- ShopTARgv23.Core/Dto/KindergartenDto.cs
+++ ShopTARgv23.Core/Dto/KindergartenDto.cs
@@ -10,16 +10,16 @@ namespace ShopTARgv23.Core.Dto
     {
         public Guid? Id { get; set; }
 
-        public string GroupName { get; set; }
+        public string? GroupName { get; set; }
 
-        public int ChildrenCount { get; set; }
+        public int? ChildrenCount { get; set; }
 
-        public string KindergartenName { get; set; }
+        public string? KindergartenName { get; set; }
 
-        public string Teacher { get; set; }
+        public string? Teacher { get; set; }
 
-        public DateTime CreatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
 
-        public DateTime UpdatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
--- ShopTARgv23/Models/Kindergartens/KindergartensIndexViewModel.cs
+++ ShopTARgv23/Models/Kindergartens/KindergartensIndexViewModel.cs
@@ -4,19 +4,18 @@ namespace ShopTARgv23.Models.Kindergartens
 {
     public class KindergartensIndexViewModel
     {
-        [Key]
         public Guid? Id { get; set; }
 
-        public string GroupName { get; set; }
+        public string? GroupName { get; set; }
 
-        public int ChildrenCount { get; set; }
+        public int? ChildrenCount { get; set; }
 
-        public string KindergartenName { get; set; }
+        public string? KindergartenName { get; set; }
 
-        public string Teacher { get; set; }
+        public string? Teacher { get; set; }
 
-        public DateTime CreatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
 
-        public DateTime UpdatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
