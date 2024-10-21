2003cf3aa0b6a8ad0dc9066f484cd2a2298b30fa
alexkaasik <aleks.kaasik@outlook.com>
2024-10-21 17:33:44 +0300

    Added some tests for update, create, delete

--- ShopTARgv23.RealEstateTest/RealEstateTest.cs
+++ ShopTARgv23.RealEstateTest/RealEstateTest.cs
@@ -1,7 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using ShopTARgv23.ApplicationService.Services;
 using ShopTARgv23.Core.Domain;
 using ShopTARgv23.Core.Dto;
 using ShopTARgv23.Core.ServiceInterface;
+using System;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace ShopTARgv23.RealEstateTest
@@ -60,10 +63,10 @@ namespace ShopTARgv23.RealEstateTest
         {
             RealEstateDto realEstate = MockRealEstateData();
 
-            var RealEstate1 = await Svc<IRealEstateServices>.Create(realEstate);
-            var RealEstate2 = await Svc<IRealEstateServices>.Create(realEstate);
+            var RealEstate1 = await Svc<IRealEstateServices>().Create(realEstate);
+            var RealEstate2 = await Svc<IRealEstateServices>().Create(realEstate);
 
-            var result = await Svc<IRealEstateServices>.Delete((Guid)RealEstate1.Id);
+            var result = await Svc<IRealEstateServices>().Delete((Guid)RealEstate1.Id);
 
             Assert.NotEqual(result.Id, RealEstate2.Id);
         }
@@ -73,11 +76,53 @@ namespace ShopTARgv23.RealEstateTest
         {
             RealEstateDto realEstate = MockRealEstateData();
 
-            var addRealEstate = await Svc<IRealEstateServices>.Create(realEstate);
+            var addRealEstate = await Svc<IRealEstateServices>().Create(realEstate);
 
-            var result = await Svc<IRealEstateServices>.Delete((Guid)addRealEstate.Id);
+            var result = await Svc<IRealEstateServices>().Delete((Guid)addRealEstate.Id);
+        }
 
-            Assert.Equal(result, addRealEstate);
+        [Fact]
+        public async Task Should_UpdataRealEstate_WhenUpdateData()
+        {
+
+            var guid = Guid.Parse("82c6af77-2c7d-4eae-8e1e-29269c3c9862");
+            RealEstateDto dto = MockRealEstateData();
+
+            RealEstate domain = new();
+
+            domain.Id = Guid.Parse("82c6af77-2c7d-4eae-8e1e-29269c3c9862");
+            domain.Location = "adsg";
+            domain.RoomNumber = 1;
+            domain.BuildingType = "asd";
+            domain.CreatedAt = DateTime.UtcNow;
+            domain.ModifiedAt = DateTime.UtcNow;
+
+            await Svc<IRealEstateServices>().Update(dto);
+
+            Assert.Equal(domain.Id, guid);
+            Assert.DoesNotMatch(domain.Location, dto.Location);
+            Assert.DoesNotMatch(domain.RoomNumber.ToString(), dto.RoomNumber.ToString());
+            Assert.NotEqual(domain.Size, dto.Size);
+            Assert.DoesNotMatch(domain.BuildingType, dto.BuildingType);
+            
+        }
+        [Fact]
+        public async Task Should_UpdataRealEstate_WhenUpdateDataVersion2()
+        {
+            RealEstateDto dto = MockRealEstateData();
+            var CreateRealEstate = await Svc< IRealEstateServices >().Create(dto);
+
+            RealEstateDto update = MockUpdateRealEstateData();
+
+            var result = await Svc<IRealEstateServices>().Update(update);
+
+            
+            Assert.DoesNotMatch(CreateRealEstate.Location, result.Location);
+            Assert.NotEqual(CreateRealEstate.ModifiedAt, result.ModifiedAt);
+            Assert.DoesNotMatch(CreateRealEstate.RoomNumber.ToString(), result.RoomNumber.ToString());
+            Assert.NotEqual(CreateRealEstate.Size, result.Size);
+            Assert.DoesNotMatch(CreateRealEstate.BuildingType, result.BuildingType);
+
         }
 
         private RealEstateDto MockRealEstateData()
@@ -93,6 +138,22 @@ namespace ShopTARgv23.RealEstateTest
             };
 
             return realEstate;
+        }
+
+        private RealEstateDto MockUpdateRealEstateData()
+        {
+            RealEstateDto realEstate = new()
+            {
+                Location = "asd",
+                Size = 100,
+                RoomNumber = 1,
+                BuildingType = "af",
+                CreatedAt = DateTime.Now,
+                ModifiedAt = DateTime.Now,
+            };
+
+            return realEstate;
+
         }
     }
 }
