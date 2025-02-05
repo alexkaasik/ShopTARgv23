c0a15895663ec4792e99b5285141b6c9e298d4ad
Aleksander Kaasik <aleks.kaasik@outlook.com>
2025-02-06 00:32:50 +0200

    Forgot Password

--- ShopTARgv23/Controllers/AccountsController.cs
+++ ShopTARgv23/Controllers/AccountsController.cs
@@ -130,5 +130,29 @@ namespace ShopTARgv23.Controllers
             };
             return View(model);
         }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user != null && await _userManager.IsEmailConfirmedAsync(user))
+                {
+                    var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+                    var passwordResetLink = Url.Action("ResetPassword", "Accounts", new { email = model.Email, token = token }, Request.Scheme);
+                    return View("ForgotPasswordConfirmation");
+                }
+                return View("ForgotPasswordConfirmation");
+            }
+            return View(model);
+        }
     }
 }
